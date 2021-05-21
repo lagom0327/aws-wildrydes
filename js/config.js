5ec6375f5bf7add885891a585f2570513f89398b
lagom032 <lagom0327@gmail.com>
2021-05-21 21:59:57 +0800

    update config

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
-    cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
-    },
-    api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
-    }
+  cognito: {
+    userPoolId: 'us-east-1_f9omAt5gl', // e.g. us-east-2_uXboG5pAb
+    userPoolClientId: '6q3grcs2oceo4qnjjthj9mgdu4', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+    region: 'us-east-1', // e.g. us-east-2
+  },
+  api: {
+    invokeUrl: '', // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+  },
 };
